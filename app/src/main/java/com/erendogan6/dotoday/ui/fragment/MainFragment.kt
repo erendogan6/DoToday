e0aa5aceb11761dbd1f0f11e793ab7dfc230cc08
erendogan6 <erendogan6@hotmail.com>
2024-03-24 09:50:41 +0300

    MainFragment Updated To A Simple Template Design

--- app/src/main/java/com/erendogan6/dotoday/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/erendogan6/dotoday/ui/fragment/MainFragment.kt
@@ -6,12 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.erendogan6.dotoday.R
+import com.erendogan6.dotoday.data.entity.ToDo
+import com.erendogan6.dotoday.ui.fragment.adaptor.ToDoAdapter
 import com.erendogan6.dotoday.databinding.FragmentMainBinding
+import com.erendogan6.dotoday.ui.fragment.viewmodel.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
+    private lateinit var viewmodel : MainViewModel
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentMainBinding.inflate(layoutInflater,container,false)
 
@@ -30,7 +37,41 @@ class MainFragment : Fragment() {
 
         })
 
+        val toDoList = ArrayList<ToDo>()
+        var todo1 = ToDo(0,"Spor","Antrenman",false,1L)
+        var todo2 = ToDo(0,"Ders","Antrenman",false,1L)
+        var todo3 = ToDo(0,"Alışveriş","Antrenman",false,1L)
+
+        toDoList.add(todo1)
+        toDoList.add(todo2)
+        toDoList.add(todo3)
+
+        val adapter = ToDoAdapter(toDoList,
+            onDeleteClicked = { position ->
+                val todoItem = toDoList[position]
+                Snackbar.make(binding.root, "Do You Want to Delete ${todoItem.title}?", Snackbar.LENGTH_LONG)
+                    .setAction("Yes") {
+                        toDoList.removeAt(position)
+                    }.show()
+            },
+            onItemClicked = { position ->
+                val todoItem = toDoList[position]
+                val action = MainFragmentDirections.actionMainFragmentToUpdateFragment(todoItem)
+                Navigation.findNavController(requireView()).navigate(action)
+            }
+        )
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.adapter = adapter
+
+
         return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tempViewModel: MainViewModel by viewModels()
+        viewmodel = tempViewModel
     }
 
 }
