b503a8f4db56d1e585c7b7e42b663ad5bc16d1b5
erendogan6 <erendogan6@hotmail.com>
2024-03-24 09:49:44 +0300

    MainActivity Updated

--- app/src/main/java/com/erendogan6/dotoday/ui/MainActivity.kt
+++ app/src/main/java/com/erendogan6/dotoday/ui/MainActivity.kt
@@ -1,10 +1,8 @@
-package com.erendogan6.dotoday
+package com.erendogan6.dotoday.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.erendogan6.dotoday.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
