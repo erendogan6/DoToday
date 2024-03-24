18b1f1fd147453170e4d8327a7ae89660471f8d1
erendogan6 <erendogan6@hotmail.com>
2024-03-24 09:40:12 +0300

    View Models Created

---
+++ app/src/main/java/com/erendogan6/dotoday/ui/fragment/viewmodel/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.erendogan6.dotoday.ui.fragment.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+}
---
+++ app/src/main/java/com/erendogan6/dotoday/ui/fragment/viewmodel/UpdateViewModel.kt
@@ -0,0 +1,6 @@
+package com.erendogan6.dotoday.ui.fragment.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class UpdateViewModel : ViewModel() {
+}
