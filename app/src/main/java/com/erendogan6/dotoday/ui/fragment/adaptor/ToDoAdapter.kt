457b2e3940cc7ee2a032b48692ef81c2e7036838
erendogan6 <erendogan6@hotmail.com>
2024-03-24 09:51:15 +0300

    ToDo RecyclerView Adaptor Created

---
+++ app/src/main/java/com/erendogan6/dotoday/ui/fragment/adaptor/ToDoAdapter.kt
@@ -0,0 +1,30 @@
+package com.erendogan6.dotoday.ui.fragment.adaptor
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.erendogan6.dotoday.data.entity.ToDo
+import com.erendogan6.dotoday.databinding.TodoCardDesignBinding
+
+class ToDoAdapter(private val dataSet: ArrayList<ToDo>,
+                  private val onDeleteClicked: (Int) -> Unit,
+                  private val onItemClicked: (Int) -> Unit) : RecyclerView.Adapter<ToDoAdapter.ViewHolder>(){
+    class ViewHolder(val binding: TodoCardDesignBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = TodoCardDesignBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.textView.text = dataSet[position].title
+        holder.binding.deleteIcon.setOnClickListener {
+            onDeleteClicked(position)
+        }
+        holder.binding.textView.setOnClickListener {
+            onItemClicked(position)
+        }
+    }
+
+    override fun getItemCount() = dataSet.size
+}
