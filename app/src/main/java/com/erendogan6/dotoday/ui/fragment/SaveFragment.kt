3dd85ef72cd4163ba52021c2c00d0b35785c3d4b
erendogan6 <erendogan6@hotmail.com>
2024-03-24 09:50:47 +0300

    SaveFragment Updated To A Simple Template Design

--- app/src/main/java/com/erendogan6/dotoday/ui/fragment/SaveFragment.kt
+++ app/src/main/java/com/erendogan6/dotoday/ui/fragment/SaveFragment.kt
@@ -5,15 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.erendogan6.dotoday.R
 import com.erendogan6.dotoday.databinding.FragmentMainBinding
 import com.erendogan6.dotoday.databinding.FragmentSaveBinding
+import com.erendogan6.dotoday.ui.fragment.viewmodel.MainViewModel
+import com.erendogan6.dotoday.ui.fragment.viewmodel.SaveViewModel
 
 class SaveFragment : Fragment() {
     private lateinit var binding: FragmentSaveBinding
+    private lateinit var viewmodel : SaveViewModel
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentSaveBinding.inflate(layoutInflater,container,false)
         return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tempViewModel: SaveViewModel by viewModels()
+        viewmodel = tempViewModel
     }
 
 }
