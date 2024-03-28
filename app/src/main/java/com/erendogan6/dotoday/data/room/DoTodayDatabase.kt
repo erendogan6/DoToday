a681d328a0ac7ce66689272cbdbc07782cefe4e5
erendogan6 <erendogan6@hotmail.com>
2024-03-28 11:47:38 +0300

    Database Edited For WorkList Feature

--- app/src/main/java/com/erendogan6/dotoday/data/room/DoTodayDatabase.kt
+++ app/src/main/java/com/erendogan6/dotoday/data/room/DoTodayDatabase.kt
@@ -3,11 +3,12 @@ package com.erendogan6.dotoday.data.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.erendogan6.dotoday.data.entity.ToDo
+import com.erendogan6.dotoday.data.model.ToDo
+import com.erendogan6.dotoday.data.model.WorkList
 import com.erendogan6.dotoday.utils.Converters
 
-@Database(entities = [ToDo::class], version = 1)
+@Database(entities = [ToDo::class, WorkList::class], version = 1)
 @TypeConverters(Converters::class)
 abstract class DoTodayDatabase : RoomDatabase() {
-    abstract fun getDao():DoTodayDao
+    abstract fun getDao(): DoTodayDao
 }
