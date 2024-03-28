27a0f57ef5f81eab6b50b641aa251d9d0ac9dc14
erendogan6 <erendogan6@hotmail.com>
2024-03-28 11:38:37 +0300

    WorkList Model Created

---
+++ app/src/main/java/com/erendogan6/dotoday/data/model/WorkList.kt
@@ -0,0 +1,13 @@
+package com.erendogan6.dotoday.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "work_list")
+data class WorkList(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "name") val name: String
+) : Serializable
