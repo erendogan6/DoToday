ebcc1498680fb273e2bb421eb7b5ba0733d08d1c
erendogan6 <erendogan6@hotmail.com>
2024-03-28 11:28:26 +0300

    Code Reformat

--- app/src/main/java/com/erendogan6/dotoday/ui/fragment/adaptor/ToDoAdapter.kt
+++ app/src/main/java/com/erendogan6/dotoday/ui/fragment/adaptor/ToDoAdapter.kt
@@ -3,16 +3,18 @@ package com.erendogan6.dotoday.ui.fragment.adaptor
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.erendogan6.dotoday.data.entity.ToDo
+import com.erendogan6.dotoday.data.model.ToDo
 import com.erendogan6.dotoday.databinding.TodoCardDesignBinding
 
-class ToDoAdapter(private val dataSet: ArrayList<ToDo>,
-                  private val onDeleteClicked: (Int) -> Unit,
-                  private val onItemClicked: (Int) -> Unit) : RecyclerView.Adapter<ToDoAdapter.ViewHolder>(){
-    class ViewHolder(val binding: TodoCardDesignBinding) : RecyclerView.ViewHolder(binding.root)
+class ToDoAdapter(
+    private val dataSet: ArrayList<ToDo>,
+    private val onDeleteClicked: (Int) -> Unit,
+    private val onItemClicked: (Int) -> Unit
+) : RecyclerView.Adapter<ToDoAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = TodoCardDesignBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            TodoCardDesignBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -27,4 +29,6 @@ class ToDoAdapter(private val dataSet: ArrayList<ToDo>,
     }
 
     override fun getItemCount() = dataSet.size
+
+    class ViewHolder(val binding: TodoCardDesignBinding) : RecyclerView.ViewHolder(binding.root)
 }
