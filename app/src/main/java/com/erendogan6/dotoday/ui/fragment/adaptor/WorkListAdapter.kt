1d8b5d458c5fb8bad13c98e0f0fa66bcb253e6c9
erendogan6 <erendogan6@hotmail.com>
2024-03-28 11:46:20 +0300

    WorkList Adaptor Added

---
+++ app/src/main/java/com/erendogan6/dotoday/ui/fragment/adaptor/WorkListAdapter.kt
@@ -0,0 +1,44 @@
+package com.erendogan6.dotoday.ui.fragment.adaptor
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.erendogan6.dotoday.data.model.WorkList
+import com.erendogan6.dotoday.databinding.WorklistCardDesignBinding
+
+class WorkListAdapter(
+    private val onDeleteClicked: (WorkList) -> Unit,
+    private val onItemClicked: (WorkList) -> Unit
+) : RecyclerView.Adapter<WorkListAdapter.WorkListViewHolder>() {
+
+    private var workLists: ArrayList<WorkList> = arrayListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WorkListViewHolder {
+        val binding =
+            WorklistCardDesignBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return WorkListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: WorkListViewHolder, position: Int) {
+        val workList = workLists[position]
+        holder.binding.WorkListName.text = workList.name
+        holder.binding.root.setOnClickListener {
+            onItemClicked(workList)
+        }
+        holder.binding.WorkListDeleteIcon.setOnClickListener {
+            onDeleteClicked(workList)
+        }
+    }
+
+    override fun getItemCount() = workLists.size
+
+    fun submitList(newWorkLists: ArrayList<WorkList>) {
+        println("adaptor submitlist calisti")
+        println(newWorkLists.size)
+        this.workLists = newWorkLists
+        notifyDataSetChanged()
+    }
+
+    class WorkListViewHolder(val binding: WorklistCardDesignBinding) :
+        RecyclerView.ViewHolder(binding.root)
+}
