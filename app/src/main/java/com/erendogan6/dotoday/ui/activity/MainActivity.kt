9409d55b460dff41c3fa1dec9b9fab2e396edb9c
erendogan6 <erendogan6@hotmail.com>
2024-03-28 11:43:48 +0300

    Added To Activity Folder

--- app/src/main/java/com/erendogan6/dotoday/ui/activity/MainActivity.kt
+++ app/src/main/java/com/erendogan6/dotoday/ui/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.erendogan6.dotoday.ui
+package com.erendogan6.dotoday.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
     }
 }
