5ac55ecda33b4638b267f57d5300d6700a53732c
erendogan6 <erendogan6@hotmail.com>
2024-03-28 11:47:29 +0300

    DataSource Edited For WorkList Feature

--- app/src/main/java/com/erendogan6/dotoday/data/datasource/DoTodayDataSource.kt
+++ app/src/main/java/com/erendogan6/dotoday/data/datasource/DoTodayDataSource.kt
@@ -1,28 +1,46 @@
 package com.erendogan6.dotoday.data.datasource
 
-import com.erendogan6.dotoday.data.entity.ToDo
+import androidx.room.Delete
+import androidx.room.Insert
+import com.erendogan6.dotoday.data.model.ToDo
+import com.erendogan6.dotoday.data.model.WorkList
 import com.erendogan6.dotoday.data.room.DoTodayDao
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class DoTodayDataSource (var doTodayDao : DoTodayDao) {
-    suspend fun save(toDo: ToDo){
+class DoTodayDataSource(var doTodayDao: DoTodayDao) {
+    suspend fun save(toDo: ToDo) {
         doTodayDao.save(toDo)
     }
 
-    suspend fun update(toDo: ToDo){
+    suspend fun update(toDo: ToDo) {
         doTodayDao.update(toDo)
     }
 
-    suspend fun delete(toDo: ToDo){
+    suspend fun delete(toDo: ToDo) {
         doTodayDao.delete(toDo)
     }
 
-    suspend fun loadToDos() : List<ToDo> = withContext(Dispatchers.IO){
-        return@withContext doTodayDao.loadToDos()
-    }
 
     suspend fun search(searchText: String): List<ToDo> {
         return doTodayDao.search(searchText)
+    }
+
+    suspend fun getToDosForWorkList(workListId: Int): List<ToDo> = withContext(Dispatchers.IO) {
+        return@withContext doTodayDao.getToDosForWorkList(workListId)
+    }
+
+    @Insert
+    suspend fun insertWorkList(workList: WorkList) {
+        return doTodayDao.insertWorkList(workList)
+    }
+
+    suspend fun getAllWorkLists(): List<WorkList> = withContext(Dispatchers.IO) {
+        return@withContext doTodayDao.getAllWorkLists()
+    }
+
+    @Delete
+    suspend fun deleteWorkList(workList: WorkList) {
+        return doTodayDao.deleteWorkList(workList)
     }
 }
