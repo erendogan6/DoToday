df7fc6369975757616fe843390cc117134c323ed
erendogan6 <erendogan6@hotmail.com>
2024-04-07 18:29:42 +0300

    Date View Added To ToDo Card Design

--- app/src/main/java/com/erendogan6/dotoday/ui/fragment/adaptor/ToDoAdapter.kt
+++ app/src/main/java/com/erendogan6/dotoday/ui/fragment/adaptor/ToDoAdapter.kt
@@ -1,18 +1,20 @@
 package com.erendogan6.dotoday.ui.fragment.adaptor
 
+import android.icu.text.SimpleDateFormat
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.erendogan6.dotoday.R
 import com.erendogan6.dotoday.data.model.ToDo
 import com.erendogan6.dotoday.databinding.TodoCardDesignBinding
+import java.util.Locale
 
-class ToDoAdapter(
-    private val dataSet: ArrayList<ToDo>,
-    private val onDeleteClicked: (Int) -> Unit,
-    private val onItemClicked: (Int) -> Unit,
-    private val onCircleClicked: (ToDo) -> Unit
-) : RecyclerView.Adapter<ToDoAdapter.ViewHolder>() {
+class ToDoAdapter(private val dataSet: ArrayList<ToDo>,
+                  private val onDeleteClicked: (Int) -> Unit,
+                  private val onItemClicked: (Int) -> Unit,
+                  private val onCircleClicked: (ToDo, View) -> Unit) :
+    RecyclerView.Adapter<ToDoAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding =
@@ -21,22 +23,14 @@ class ToDoAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.titleText.text = dataSet[position].title
-
-        if (dataSet[position].isCompleted) {
-            holder.binding.completeIcon.setImageResource(R.drawable.check_circle_icon)
-        } else {
-            holder.binding.completeIcon.setImageResource(R.drawable.circle_icon)
-        }
-
-        holder.binding.deleteIcon.setOnClickListener {
-            onDeleteClicked(position)
-        }
-        holder.binding.root.setOnClickListener {
-            onItemClicked(position)
-        }
-        holder.binding.completeIcon.setOnClickListener {
-            onCircleClicked(dataSet[position])
+        with(holder.binding) {
+            titleText.text = dataSet[position].title
+            duedateText.text = SimpleDateFormat("yyyy-MM-dd",
+                                                Locale.getDefault()).format(dataSet[position].dueDate)
+            completeIcon.setImageResource(if (dataSet[position].isCompleted) R.drawable.check_circle_icon else R.drawable.circle_icon)
+            deleteIcon.setOnClickListener { onDeleteClicked(position) }
+            root.setOnClickListener { onItemClicked(position) }
+            completeIcon.setOnClickListener { onCircleClicked(dataSet[position], holder.itemView) }
         }
     }
 
