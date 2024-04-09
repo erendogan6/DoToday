ab008c3073e43dd8cbc8df62098d5814c26866ca
erendogan6 <erendogan6@hotmail.com>
2024-04-09 18:00:35 +0300

    Optimize ToDoListAdapter for performance and readability improvements

--- app/src/main/java/com/erendogan6/dotoday/ui/fragment/adaptor/ToDoAdapter.kt
+++ app/src/main/java/com/erendogan6/dotoday/ui/fragment/adaptor/ToDoAdapter.kt
@@ -10,15 +10,12 @@ import com.erendogan6.dotoday.data.model.ToDo
 import com.erendogan6.dotoday.databinding.TodoCardDesignBinding
 import java.util.Locale
 
-class ToDoAdapter(private val dataSet: ArrayList<ToDo>,
-                  private val onDeleteClicked: (Int) -> Unit,
-                  private val onItemClicked: (Int) -> Unit,
-                  private val onCircleClicked: (ToDo, View) -> Unit) :
-    RecyclerView.Adapter<ToDoAdapter.ViewHolder>() {
+class ToDoAdapter(private val dataSet: ArrayList<ToDo>, private val onDeleteClicked: (ToDo) -> Unit, private val onItemClicked: (ToDo) -> Unit,
+    private val onCircleClicked: (ToDo, View) -> Unit) : RecyclerView.Adapter<ToDoAdapter.ViewHolder>() {
+    private val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding =
-            TodoCardDesignBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = TodoCardDesignBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -26,27 +23,31 @@ class ToDoAdapter(private val dataSet: ArrayList<ToDo>,
         val todoItem = dataSet[position]
 
         with(holder.binding) {
-            titleText.text = dataSet[position].title
-            if (todoItem.dueDate != null) {
-                duedateText.visibility = View.VISIBLE
-                duedateText.text =
-                    SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(todoItem.dueDate)
-            } else {
-                duedateText.visibility = View.GONE
+            titleText.text = todoItem.title
+            duedateText.apply {
+                if (todoItem.dueDate != null) {
+                    visibility = View.VISIBLE
+                    text = dateFormat.format(todoItem.dueDate)
+                } else {
+                    visibility = View.GONE
+                }
             }
-            if (todoItem.reminderDate != null) {
-                alarmIcon.visibility = View.VISIBLE
-            } else {
-                alarmIcon.visibility = View.INVISIBLE
-            }
-            completeIcon.setImageResource(if (dataSet[position].isCompleted) R.drawable.check_circle_icon else R.drawable.circle_icon)
-            deleteIcon.setOnClickListener { onDeleteClicked(position) }
-            root.setOnClickListener { onItemClicked(position) }
-            completeIcon.setOnClickListener { onCircleClicked(dataSet[position], holder.itemView) }
+            alarmIcon.visibility = if (todoItem.reminderDate != null) View.VISIBLE else View.GONE
+
+            completeIcon.setImageResource(if (todoItem.isCompleted) R.drawable.check_circle_icon else R.drawable.circle_icon)
+            deleteIcon.setOnClickListener { onDeleteClicked(todoItem) }
+            root.setOnClickListener { onItemClicked(todoItem) }
+            completeIcon.setOnClickListener { onCircleClicked(todoItem, holder.itemView) }
         }
     }
 
     override fun getItemCount() = dataSet.size
+
+    fun updateData(newData: List<ToDo>) {
+        dataSet.clear()
+        dataSet.addAll(newData)
+        notifyDataSetChanged()
+    }
 
     class ViewHolder(val binding: TodoCardDesignBinding) : RecyclerView.ViewHolder(binding.root)
 }
