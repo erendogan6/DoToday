c3a2176304b346bc3023002c9e230d8ca17f28ad
erendogan6 <erendogan6@hotmail.com>
2024-04-11 00:06:32 +0300

    Add Toast Messages For Eisenhower Matrix

--- app/src/main/java/com/erendogan6/dotoday/ui/fragment/EisenHowerMatrixFragment.kt
+++ app/src/main/java/com/erendogan6/dotoday/ui/fragment/EisenHowerMatrixFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.erendogan6.dotoday.databinding.FragmentEisenHoverBinding
@@ -33,19 +34,27 @@ class EisenHowerMatrixFragment : Fragment() {
     private fun FragmentEisenHoverBinding.setupClickListeners() {
         importantAndUrgent.setOnClickListener {
             navigateToListFragment("important_and_urgent")
+            showToast("Do the task yourself immediately")
         }
 
         importantAndNotUrgent.setOnClickListener {
             navigateToListFragment("important_and_not_urgent")
+            showToast("Plan the task and complete it yourself")
         }
 
         notImportantAndUrgent.setOnClickListener {
             navigateToListFragment("not_important_and_urgent")
+            showToast("Delegate/Automate, if not possible, do it yourself after a time")
         }
 
         notImportantAndNotUrgent.setOnClickListener {
             navigateToListFragment("not_important_and_not_urgent")
+            showToast("Forget it and come back later")
         }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireActivity().applicationContext, message, Toast.LENGTH_LONG).show()
     }
 
     private fun navigateToListFragment(categoryName: String) {
